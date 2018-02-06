7894c8b2d952113170acd67573c84330ba3e0830
Cody Welsh <c.welsh@codelitt.com>
2018-02-05 17:31:50 -0800

    feat(App): Remedial Webpack Setup
    This commit contains a base configuration for Webpack in the
    application. I'm assuming a rather simple setup for React, without
    paying too much attention to production at this stage.

---
+++ config/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const extractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new extractTextPlugin({
+  filename: "[name].[contenthash].css"
+});
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader"
+        }]
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  plugins: [
+    extractSass
+  ]
+};
+
+module.exports = config;
